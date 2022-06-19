271d32092b69dc2ab41534a0e8075f7b54968d18
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2022-06-19 21:24:24 +0700

    delete unused var

--- src/main/java/binus/assignment/Two.java
+++ src/main/java/binus/assignment/Two.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Two {
 
     public static void main(String[] args) {
-        int i = 2, chk, count = 0, res = 0, n;
+        int i = 2, res = 0, n;
         
         Scanner scan = new Scanner(System.in);
         System.out.print("Masukkan N : ");
