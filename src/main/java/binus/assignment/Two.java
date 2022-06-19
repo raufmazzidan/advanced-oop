f7251812edefbd74981a1fb8eb8e8e3282dac195
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2022-06-19 21:21:56 +0700

    two

---
+++ src/main/java/binus/assignment/Two.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package binus.assignment;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author raufmazzidan
+ */
+public class Two {
+
+    public static void main(String[] args) {
+        int i = 2, chk, count = 0, res = 0, n;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan N : ");
+        n = scan.nextInt();
+        
+        System.out.print("Deret : ");
+        while(n > 0) {
+            int totalFactor = 0;
+            
+            for (int k = 1; k <= i; k++) {
+                if (i % k == 0) {
+                    totalFactor++;
+                }
+            }
+            if (totalFactor == 2) {
+                System.out.print(i + ", ");
+                n--;
+                res += i;
+            }
+            i++;
+        }
+       
+        System.out.println("\nTotal : " + res);
+    }
+}
